4cfe70e8f6bce67a942deed46f97898a92a41bac
Josephat <josephat.edu@gmail.com>
2022-07-13 18:20:12 +0100

    deployment job

--- .circleci/ansible/roles/configure-server/files/env.sh
+++ .circleci/ansible/roles/configure-server/files/env.sh
@@ -5,10 +5,8 @@ export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 
 # Use these values for the local PG database from the Docker Compose file
-export TYPEORM_HOST=database-1.ckjddnywftwz.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=udapeople-prod.ckjddnywftwz.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=TYPEORM_PASSWORD
+export TYPEORM_PASSWORD=Josephat09
 export TYPEORM_DATABASE=postgres
-#export check="{{lookup('env', TYPEORM_HOST)}}"
-
