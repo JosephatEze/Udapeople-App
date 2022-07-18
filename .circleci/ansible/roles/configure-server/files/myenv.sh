858919020f5d564659dacb44eb625422ae0c0c26
Josephat <josephat.edu@gmail.com>
2022-07-18 16:48:00 +0100

    clean up job

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,6 +1,6 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=udapeople-prod.ckjddnywftwz.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=database-1.cgkdrvecbtim.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=Josephat09
